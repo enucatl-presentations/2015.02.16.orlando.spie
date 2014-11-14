bdfb103ca3fbfdb18d86796ea32638180490247d
Matteo Abis <gmatteo..abis@gmail.com>
2014-11-14 14:55:55 +0100

    reorganized with newest noise model

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,10 +1,6 @@
 //= require vendor/jquery-2.1.1.min
 //= require vendor/d3.min
-//= require vendor/d3.line/line
 //= require vendor/d3.scatter/scatter
 //= require vendor/reveal.js/reveal.min
-//= require _bisect
 //= require _init_reveal
 //= require _ratio
-//= require _sigma-absorption
-//= require _max-thickness-table
